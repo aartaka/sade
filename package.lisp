f51d81b89e5f9ff8e74f797f0a9610697f9ac47d
aartaka <aartaka@protonmail.com>
2022-05-08 22:42:39 +0500

    package: Export all the useful stuff.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,7 @@
 ;;;; package.lisp
 
 (defpackage #:bfl
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:bf #:bf-compile
+           #:bf-compile-from-file #:bf-compile-from-string
+           #:bf-eval-file #:bf-eval-string))
